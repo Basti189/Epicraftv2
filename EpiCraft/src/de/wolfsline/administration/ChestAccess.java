b265fdbd7aaaf5bd195df3712f24c6af7711ded1
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-09 16:02:48 +0200

    Einsicht des gesamten Inventars

--- EpiCraft/src/de/wolfsline/administration/ChestAccess.java
+++ EpiCraft/src/de/wolfsline/administration/ChestAccess.java
@@ -6,6 +6,7 @@ import org.bukkit.block.Chest;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import de.wolfsline.Epicraft.Epicraft;
@@ -33,7 +34,7 @@ public class ChestAccess implements CommandExecutor{
 		Block block = p.getTargetBlock(null, 200).getLocation().getBlock();
 		if(block.getState() instanceof Chest){
 			Chest chest = ((Chest) block.getState());
-			p.openInventory(chest.getInventory()); //Vorher getBlockInventory
+			p.openInventory(chest.getInventory());
 			this.plugin.api.sendLog("[Epicraft - Kistenzugiff] " + p.getName() + " greift auf eine Kiste zu");
 			return true;
 		}
