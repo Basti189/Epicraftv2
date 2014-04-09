8ca86387f5ac837e79b4c4f8e51482aac04ba231
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-09 18:42:49 +0200

    Überarbeitet

--- EpiCraft/src/de/wolfsline/modify/GamemodeListener.java
+++ EpiCraft/src/de/wolfsline/modify/GamemodeListener.java
@@ -1,24 +1,18 @@
 package de.wolfsline.modify;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Egg;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public class GamemodeListener implements Listener{
-	private Player p;
-	private Player p2;
 	
 	@EventHandler
 	public void onPlayerGamemodeChange(PlayerGameModeChangeEvent event){
-		p = event.getPlayer();
+		final Player p = event.getPlayer();
 		if(p.hasPermission("epicraft.fly.use"))
 			return;
 		new Thread(new Runnable() {
@@ -36,22 +30,12 @@ public class GamemodeListener implements Listener{
 	
 	@EventHandler
 	public void onPlayerLoginEvent(PlayerLoginEvent event){
-		p2 = event.getPlayer();
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Player myPlayer = p2;
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-				}
-				myPlayer.setAllowFlight(false);
-			}
-		}).start();
+		Player p = event.getPlayer();
+		p.setAllowFlight(false);
 	}
 	
 	@EventHandler
-	public void onPlayreInteractEvent(PlayerInteractEvent event){
+	public void onPlayerInteractEvent(PlayerInteractEvent event){
 		Player p = event.getPlayer();
 		if(p.isOp())
 			return;
