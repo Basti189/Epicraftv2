304a9711e1d140b7b494a4b7a7e927fc850f244b
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-13 00:08:40 +0200

    Fatalen Bug verbessert

--- EpiCraft/src/de/wolfsline/forfun/PVP.java
+++ EpiCraft/src/de/wolfsline/forfun/PVP.java
@@ -3,7 +3,6 @@ package de.wolfsline.forfun;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -17,6 +16,7 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
@@ -66,14 +66,11 @@ public class PVP implements CommandExecutor, Listener {
 		}
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onDamage(EntityDamageByEntityEvent event){
 		Entity e = event.getEntity();
 		Entity d = event.getDamager();
-		if(!(e instanceof Player && (d instanceof Player || d instanceof Arrow || d instanceof Fish) || d instanceof Egg || d instanceof Snowball)){
-			return;
-		}
-		if(e instanceof Horse){
+		if(!(e instanceof Player && (d instanceof Player || d instanceof Arrow || d instanceof Fish || d instanceof Egg || d instanceof Snowball))){
 			return;
 		}
 		
