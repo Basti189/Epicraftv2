81364c25596f66ed39391de3f55e12390e068b16
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-13 02:48:21 +0200

    Klasse AFK implementiert

---
+++ EpiCraft/src/de/wolfsline/afk/AFK.java
@@ -0,0 +1,14 @@
+package de.wolfsline.afk;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class AFK implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		return false;
+	}
+
+}
