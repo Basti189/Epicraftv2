cc7b731ae681aa6661c650d4f1e9578c1bffbc4c
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-20 00:32:48 +0200

    Zugriff auf Epicraft-Instance gewährt

--- EpiCraft/src/de/wolfsline/security/CreatureSpawnListener.java
+++ EpiCraft/src/de/wolfsline/security/CreatureSpawnListener.java
@@ -4,11 +4,20 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+
+import de.wolfsline.Epicraft.Epicraft;
 
 public class CreatureSpawnListener implements Listener{
 	
+	private Epicraft plugin;
+	
 	private final String WORLD = "world";
 	
+	public CreatureSpawnListener(Epicraft plugin) {
+		this.plugin = plugin;
+	}
+
 	@EventHandler
 	public void onCreatureSpawnListener(CreatureSpawnEvent event){
 		if(event.getEntityType() == EntityType.BAT)//Fledermäuse
