7bf78fdcdbd4bc551ee1be09a411c30623bcc94d
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-20 00:54:28 +0200

    BugFixes

--- EpiCraft/src/de/wolfsline/administration/InventarCommand.java
+++ EpiCraft/src/de/wolfsline/administration/InventarCommand.java
@@ -26,7 +26,7 @@ public class InventarCommand implements CommandExecutor {
         Player p = (Player) cs;
         if(!p.hasPermission("epicraft.inv")){
             p.sendMessage(plugin.error);
-            plugin.api.sendLog("[Epicraft - Inventar] " + p.getName() + " versuchte auf das Inventar von " + args[0] + " zuzugreifen");
+            plugin.api.sendLog("[Epicraft - Inventar] " + p.getName() + " versucht auf den Befehl zuzugreifen");
             return true;
         }
         if(args.length < 1){
