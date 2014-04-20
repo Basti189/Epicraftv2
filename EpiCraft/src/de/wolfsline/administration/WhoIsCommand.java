61151e9775e2f64c70779a4fcc014c2ad66ee814
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-20 23:20:10 +0200

    Bereit für MC 1.8

--- EpiCraft/src/de/wolfsline/administration/WhoIsCommand.java
+++ EpiCraft/src/de/wolfsline/administration/WhoIsCommand.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -42,29 +43,39 @@ public class WhoIsCommand implements CommandExecutor{
 			p.sendMessage(plugin.namespace + ChatColor.WHITE + "Anfrage wird verarbeitet - Bitte warten...");
 			final String who = args[0];
 			final Player whoP = p;
-			Player player = Bukkit.getServer().getPlayer(who);
+			UUID targetUUID = plugin.uuid.getUUIDFromPlayer(who);
+			if(targetUUID == null){
+				p.sendMessage(plugin.uuid.ERROR);
+				return true;
+			}
+			Player player = Bukkit.getServer().getPlayer(targetUUID);
 			if(player != null){
 				new Thread(new Runnable() {
 					@Override
 					public void run() {
 						Player p = whoP;
-						Player player = Bukkit.getServer().getPlayer(who);
+						UUID targetUUID = plugin.uuid.getUUIDFromPlayer(who);
+						if(targetUUID == null){
+							p.sendMessage(plugin.uuid.ERROR);
+							return;
+						}
+						Player player = Bukkit.getServer().getPlayer(targetUUID);
 						if(player != null){
 							String country = "Unbekannt";
-							String city = "Unbekannt";
+							//String city = "Unbekannt";
 							try {
 								JSONObject jsonObject = JSONReader.readJsonFromUrl("http://freegeoip.net/json/" + player.getAddress().getHostString());
 								country = (String) jsonObject.get("country_name");
-								city = (String) jsonObject.get("city");
+								//city = (String) jsonObject.get("city");
 							} catch (IOException | ParseException e) {
 							}
 							
 							long firstPlayed = player.getFirstPlayed();
 							long lastPlayed = player.getLastPlayed();
-							long timePlayed = player.getPlayerTime();
+							//long timePlayed = player.getPlayerTime();
 							Date dateFirstPlayed = new Date(firstPlayed);
 							Date dateLastPlayed = new Date(lastPlayed);
-							Date dateTimePlayed = new Date(timePlayed);
+							//Date dateTimePlayed = new Date(timePlayed);
 							
 							SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.GERMANY);
 							SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY);
@@ -76,7 +87,7 @@ public class WhoIsCommand implements CommandExecutor{
 							p.sendMessage(ChatColor.GOLD + "Position: " + ChatColor.WHITE + (int)p.getLocation().getX() + " " + (int)p.getLocation().getY() + " " + (int)p.getLocation().getZ());
 							p.sendMessage(ChatColor.GOLD + "Erster Login: am " + ChatColor.WHITE + dateFormat.format(dateFirstPlayed) + ChatColor.GOLD + " um " + ChatColor.WHITE + timeFormat.format(dateFirstPlayed));
 							p.sendMessage(ChatColor.GOLD + "Letzter Login: am " + ChatColor.WHITE + dateFormat.format(dateLastPlayed) + ChatColor.GOLD + " um " + ChatColor.WHITE + timeFormat.format(dateLastPlayed));
-							p.sendMessage(ChatColor.GOLD + "Spielzeit: " + ChatColor.WHITE + (timePlayed / 1000 / 60) + ChatColor.GOLD + " Min"); //<-- Muss umgerechnet werden
+							//p.sendMessage(ChatColor.GOLD + "Spielzeit: " + ChatColor.WHITE + (timePlayed / 1000 / 60) + ChatColor.GOLD + " Min"); //<-- Muss umgerechnet werden
 							p.sendMessage(ChatColor.GOLD + "Standort: " + ChatColor.WHITE + country.replace("Germany", "Deutschland"));
 							p.sendMessage(ChatColor.GOLD + "---------------[Information]---------------");
 						}
@@ -84,7 +95,12 @@ public class WhoIsCommand implements CommandExecutor{
 				}).start();
 			}
 			else{
-				OfflinePlayer offPlayer = Bukkit.getServer().getOfflinePlayer(args[0]);
+				UUID offlineUUID = plugin.uuid.getUUIDFromPlayer(args[0]);
+				if(offlineUUID == null){
+					p.sendMessage(plugin.uuid.ERROR);
+					return true;
+				}
+				OfflinePlayer offPlayer = Bukkit.getServer().getOfflinePlayer(offlineUUID);
 				if(!offPlayer.hasPlayedBefore()){
 					p.sendMessage(plugin.namespace + ChatColor.RED + "Dieser Spieler ist auf dem Server nicht bekannt!");
 					return true;
