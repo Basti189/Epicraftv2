cce3f8661fc7ed5121624a0e29a119c1b4634b09
Sebastian Wolf <wolfsline@googlemail.com>
2014-04-22 21:37:55 +0200

    Stehendes Schild eingefügt und neue Directions

--- EpiCraft/src/de/wolfsline/teleport/SignLift.java
+++ EpiCraft/src/de/wolfsline/teleport/SignLift.java
@@ -1,10 +1,13 @@
 package de.wolfsline.teleport;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -28,13 +31,24 @@ public class SignLift implements Listener{
 	public void onSignChangeEvent(SignChangeEvent event){
 		if(event.getLine(0).equalsIgnoreCase("[Lift]")){
 			if(!(event.getPlayer().isOp() || event.getPlayer().hasPermission("epicraft.sign.lift.create"))){
-				if(event.getPlayer().getLocation().getWorld().getName().equalsIgnoreCase("plots")){
-					return;
-				}
 				event.setCancelled(true);
 				event.getBlock().breakNaturally();
 				event.getPlayer().sendMessage(plugin.namespace + ChatColor.RED + "Du hast keinen Zugriff auf den Lift!");
-			}	
+			}
+			String direction = ((Directional)event.getBlock().getType().getNewData(event.getBlock().getData())).getFacing().toString();
+			if(!(direction.equalsIgnoreCase("north") ||
+					direction.equalsIgnoreCase("east") || 
+					direction.equalsIgnoreCase("south") || 
+					direction.equalsIgnoreCase("west") || 
+					direction.equalsIgnoreCase("north_west") || 
+					direction.equalsIgnoreCase("south_west") || 
+					direction.equalsIgnoreCase("south_east") || 
+					direction.equalsIgnoreCase("north_east"))){
+				event.setLine(0, ChatColor.RED + "Richtung:");
+				event.setLine(1, ChatColor.WHITE + direction);
+				event.setLine(2, ChatColor.RED + "nicht");
+				event.setLine(3, ChatColor.RED + "unterstützt!");
+			}
 		}
 	}
 
@@ -45,7 +59,7 @@ public class SignLift implements Listener{
 			return;
 		if(!(event.getClickedBlock() instanceof Block))
 			return;
-		if (event.getClickedBlock().getType() == Material.WALL_SIGN && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+		if ((event.getClickedBlock().getType() == Material.WALL_SIGN || event.getClickedBlock().getType() == Material.SIGN_POST) && event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			Sign sign = (Sign) event.getClickedBlock().getState();
 			String line1 = ChatColor.stripColor(sign.getLine(0));
 			if(line1.equalsIgnoreCase("[Lift]")){
@@ -58,7 +72,7 @@ public class SignLift implements Listener{
 					int z = loc.getBlockZ();
 					for(int i = y + 1 ; i < 250 ; i++){
 						Block block = new Location(loc.getWorld(), x, i, z).getBlock();
-						if(block.getType() == Material.WALL_SIGN){
+						if(block.getType() == Material.WALL_SIGN || block.getType() == Material.SIGN_POST){
 							Sign mySign = (Sign) block.getState();
 							if(ChatColor.stripColor(mySign.getLine(0)).equalsIgnoreCase("[Lift]")){
 								p.teleport(getLocationInFrontSign(block));
@@ -78,7 +92,7 @@ public class SignLift implements Listener{
 					int z = loc.getBlockZ();
 					for(int i = y - 1 ; i > 10 ; i--){
 						Block block = new Location(loc.getWorld(), x, i, z).getBlock();
-						if(block.getType() == Material.WALL_SIGN){
+						if(block.getType() == Material.WALL_SIGN || block.getType() == Material.SIGN_POST){
 							Sign mySign = (Sign) block.getState();
 							if(ChatColor.stripColor(mySign.getLine(0)).equalsIgnoreCase("[Lift]")){
 								p.teleport(getLocationInFrontSign(block));
@@ -93,7 +107,7 @@ public class SignLift implements Listener{
 				}
 			}
 		}
-		if (event.getClickedBlock().getType() == Material.WALL_SIGN && event.getAction() == Action.LEFT_CLICK_BLOCK) {
+		if ((event.getClickedBlock().getType() == Material.WALL_SIGN || event.getClickedBlock().getType() == Material.SIGN_POST) && event.getAction() == Action.LEFT_CLICK_BLOCK) {
 			Sign sign = (Sign) event.getClickedBlock().getState();
 			String lineLift = ChatColor.stripColor(sign.getLine(0));
 			if(lineLift.equalsIgnoreCase("[Lift]")){
@@ -106,7 +120,7 @@ public class SignLift implements Listener{
 					int z = loc.getBlockZ();
 					for(int i = y - 1 ; i > 10 ; i--){
 						Block block = new Location(loc.getWorld(), x, i, z).getBlock();
-						if(block.getType() == Material.WALL_SIGN){
+						if(block.getType() == Material.WALL_SIGN || block.getType() == Material.SIGN_POST){
 							Sign mySign = (Sign) block.getState();
 							if(ChatColor.stripColor(mySign.getLine(0)).equalsIgnoreCase("[Lift]")){//Lift runter gefunden
 								sign.setLine(2, lineDirWithColor.replace("hoch", "runter"));
@@ -124,7 +138,7 @@ public class SignLift implements Listener{
 					int z = loc.getBlockZ();
 					for(int i = y + 1 ; i < 250 ; i++){
 						Block block = new Location(loc.getWorld(), x, i, z).getBlock();
-						if(block.getType() == Material.WALL_SIGN){
+						if(block.getType() == Material.WALL_SIGN || block.getType() == Material.SIGN_POST){
 							Sign mySign = (Sign) block.getState();
 							if(ChatColor.stripColor(mySign.getLine(0)).equalsIgnoreCase("[Lift]")){
 								sign.setLine(2, lineDirWithColor.replace("runter", "hoch"));
@@ -142,25 +156,42 @@ public class SignLift implements Listener{
 	private Location getLocationInFrontSign(Block block){
 		String direction = ((Directional)block.getType().getNewData(block.getData())).getFacing().toString();
 		Location loc = new Location(block.getWorld(), block.getX(), block.getY() - 1, block.getZ());
+		//Bukkit.getServer().broadcastMessage("Direction: " + direction);
 		if(direction.equalsIgnoreCase("north")){
 			loc.setZ(loc.getZ() - 1.0); //-1.5
 			loc.setX(loc.getX() + 0.5);
-			loc.setYaw(0);
+			loc.setYaw(0.0F);
 		}
 		else if(direction.equalsIgnoreCase("east")){
 			loc.setX(loc.getX() + 2.0); //+.1.5
 			loc.setZ(loc.getZ() + 0.5);
-			loc.setYaw(90);
+			loc.setYaw(90.0F);
 		}
 		else if(direction.equalsIgnoreCase("south")){
 			loc.setZ(loc.getZ() + 2.0); // +1.5
 			loc.setX(loc.getX() + 0.5);
-			loc.setYaw(180);
+			loc.setYaw(180.0F);
 		}
 		else if(direction.equalsIgnoreCase("west")){
 			loc.setX(loc.getX() - 1.0); // -1.5
 			loc.setZ(loc.getZ() + 0.5);
-			loc.setYaw(-90);
+			loc.setYaw(-90.0F);
+		}
+		else if(direction.equalsIgnoreCase("south_west")){
+			loc.setX(loc.getX() - 0.5);
+			loc.setZ(loc.getZ() + 1.5);
+			loc.setYaw(-135.0F);
+		}
+		else if(direction.equalsIgnoreCase("south_east")){
+			loc.setX(loc.getX() + 1.5);
+			loc.setZ(loc.getZ() + 1.5);
+			loc.setYaw(+135.0F);
+		}
+		else if(direction.equalsIgnoreCase("north_east")){
+			
+		}
+		else if(direction.equalsIgnoreCase("north_west")){
+			
 		}
 		return loc;
 	}
