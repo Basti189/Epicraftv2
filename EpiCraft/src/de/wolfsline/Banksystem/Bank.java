b165176ff4f57187522c87a40ef621d9eb2060bc
Sebastian Wolf <wolfsline@googlemail.com>
2014-05-02 13:44:05 +0200

    Grundsystem erstellt

---
+++ EpiCraft/src/de/wolfsline/Banksystem/Bank.java
@@ -0,0 +1,14 @@
+package de.wolfsline.Banksystem;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Bank implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) { 
+		return false;
+	}
+
+}
